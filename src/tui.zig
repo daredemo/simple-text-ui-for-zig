00bfb718dbf877f4e1da3fce05f6c9228253f60f
René <daredemoikari@gmail.com>
2024-11-11 02:26:46 +0200

    with cTermio.zig

--- src/tui.zig
+++ src/tui.zig
@@ -2,12 +2,13 @@ pub const Term = @import("ansi_terminal.zig");
 pub const Border = @import("Border.zig");
 pub const CharReader = @import("CharReader.zig");
 pub const Color = @import("Color.zig");
-pub const LibDef = @import("definitions.zig");
+// pub const LibDef = @import("definitions.zig");
 pub const Location = @import("Location.zig");
 pub const Panel = @import("Panel.zig");
 pub const BuffWriter = @import("SimpleBufferedWriter.zig");
 pub const StringStuff = @import("StringStuff.zig");
 pub const TextLine = @import("TextLine.zig");
+pub const CStuff = @import("cTermio.zig");
 
 // comptime {
 //     _ = Term;
